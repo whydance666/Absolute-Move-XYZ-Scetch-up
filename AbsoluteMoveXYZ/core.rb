f05de84089e5d40052d4dc01f764726cf9c39b1a
whydance666 <maliutin.andrei2016@yandex.ru>
2026-02-22 21:09:53 +0300

    core v2.1

--- AbsoluteMoveXYZ/core.rb
+++ AbsoluteMoveXYZ/core.rb
@@ -16,6 +16,19 @@ module AbsoluteMoveXYZ
     HTML
   end
 
+  # Блок выбора точки привязки объекта
+  def self.anchor_block
+    <<-HTML
+      <label>Anchor:</label>
+      <select id="anchor">
+        <option value="bottom">Bottom</option>
+        <option value="center" selected>Center</option>
+        <option value="top">Top</option>
+      </select>
+      <br>
+    HTML
+  end
+
   # Создание диалога
   def self.create_dialog
     @dialog = UI::HtmlDialog.new(
@@ -24,7 +37,7 @@ module AbsoluteMoveXYZ
       scrollable: false,
       resizable: false,
       width: 420,
-      height: 380,
+      height: 420,
       style: UI::HtmlDialog::STYLE_DIALOG
     )
 
@@ -37,6 +50,7 @@ module AbsoluteMoveXYZ
         #{axis_block("Y")}
         #{axis_block("Z")}
         <br>
+        #{anchor_block}
         <div style="display:flex; gap:10px;">
           <button onclick="applyData(false)" style="flex:1;height:40px;"> Apply </button>
           <button onclick="applyData(true)" style="flex:1;height:40px;"> OK </button>
@@ -49,7 +63,8 @@ module AbsoluteMoveXYZ
               y: document.getElementById("y_value").value,
               y_mode: document.getElementById("y_mode").value,
               z: document.getElementById("z_value").value,
-              z_mode: document.getElementById("z_mode").value
+              z_mode: document.getElementById("z_mode").value,
+              anchor: document.getElementById("anchor").value
             };
             window.apply(JSON.stringify(data));
             if(closeDialog){
@@ -83,14 +98,38 @@ module AbsoluteMoveXYZ
     x = data["x"].to_f
     y = data["y"].to_f
     z = data["z"].to_f
-    target = Geom::Point3d.new(x, y, z)
+    anchor = data["anchor"] || "center"
 
     model.start_operation("Move to Absolute XYZ", true)
 
     selection.each do |entity|
+      # Определяем точку перемещения по anchor
       bb = entity.bounds
-      origin = bb.min
-      vector = target - origin
+      origin = case anchor
+               when "bottom"
+                 Geom::Point3d.new(bb.center.x, bb.center.y, bb.min.z)
+               when "top"
+                 Geom::Point3d.new(bb.center.x, bb.center.y, bb.max.z)
+               else # center
+                 bb.center
+               end
+
+      # Для групп и компонентов используем их реальную origin
+      if entity.is_a?(Sketchup::Group) || entity.is_a?(Sketchup::ComponentInstance)
+        origin = entity.transformation.origin
+        # Сдвиг по оси Z в зависимости от выбранного anchor
+        z_offset = case anchor
+                   when "bottom" then bb.min.z - bb.center.z
+                   when "top"    then bb.max.z - bb.center.z
+                   else 0
+                   end
+        target_point = Geom::Point3d.new(x, y, z - z_offset)
+        vector = target_point - origin
+      else
+        target_point = Geom::Point3d.new(x, y, z)
+        vector = target_point - origin
+      end
+
       entity.transform!(Geom::Transformation.translation(vector))
     end
 
