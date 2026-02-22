1b4680dc4f6c100911cf36412f208a5443004355
whydance <57824530+whydance666@users.noreply.github.com>
2026-02-22 17:38:37 +0300

    Create absolute_move_xyz.rb

---
+++ absolute_move_xyz.rb
@@ -0,0 +1,19 @@
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module AbsoluteMoveXYZ
+  EXTENSION_NAME = "Absolute Move XYZ"
+  EXTENSION_VERSION = "2.0.0"
+
+  unless file_loaded?(__FILE__)
+    loader = File.join(File.dirname(__FILE__), 'AbsoluteMoveXYZ', 'core.rb')
+
+    extension = SketchupExtension.new(EXTENSION_NAME, loader)
+    extension.description = "Move objects using absolute XYZ coordinates with per-axis reference control."
+    extension.version     = EXTENSION_VERSION
+    extension.creator     = "Your Name"
+
+    Sketchup.register_extension(extension, true)
+    file_loaded(__FILE__)
+  end
+end
